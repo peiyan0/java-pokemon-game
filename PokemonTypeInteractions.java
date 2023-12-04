2faba0cc9fe0d8eeba19afa44cfee05df69512fd
myleslim <111961610+myleslim@users.noreply.github.com>
2023-12-04 14:40:43 +0800

    Update PokemonTypeInteractions.java

--- PokemonTypeInteractions.java
+++ PokemonTypeInteractions.java
@@ -7,11 +7,11 @@ enum PokemonType {
 public class PokemonTypeInteractions {
     // Define effectiveness matrix
     private static double[][] effectiveness = {
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0, 1, 1, 0.5, 1}, // NORMAL
-        {1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5, 1, 2, 1}, // FIRE
-        {1, 2, 0.5, 1, 0.5, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5, 1, 1, 1}, // WATER
-        {1, 1, 2, 0.5, 0, 1, 1, 1, 2, 2, 1, 1, 1, 1, 0.5, 1, 1, 1}, // ELECTRIC
-        {1, 0.5, 2, 1, 0.5, 1, 1, 0.5, 0, 2, 1, 0.5, 2, 1, 0.5, 1, 0.5, 1}, // GRASS
+        {1, 1, 1, 1, 1}, // NORMAL
+        {1, 0.5, 0.5, 1, 2}, // FIRE
+        {1, 2, 0.5, 1, 0.5}, // WATER
+        {1, 1, 2, 0.5, 0}, // ELECTRIC
+        {1, 0.5, 2, 1, 0.5}, // GRASS
     };
 
     // Method to get effectiveness of an attack from one type to another
