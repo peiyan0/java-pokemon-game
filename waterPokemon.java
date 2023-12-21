ec8333ee2e24c697c9a7c77ede225d57f770064c
peiyan0 <138883583+peiyan0@users.noreply.github.com>
2023-12-21 14:30:11 +0800

    Update waterPokemon.java

--- waterPokemon.java
+++ waterPokemon.java
@@ -2,4 +2,8 @@ public class waterPokemon extends Pokemon {
     public waterPokemon(String name, int hp, String attackName, int attackDamage) {
         super(name, "Water", hp, attackName, attackDamage);
     }
+    @Override
+    public String toString() {
+        return String.format("%s (Water) - HP: %d, Water Move: %s (%d)", getName(), getHp(), getAttackName(), getAttackDamage());
+    }
 }
